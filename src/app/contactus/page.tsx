dd0ce032f4b3d3115fa096b98e95cc2551efcd75
LuisCldrn <Luis.cldrn@yahoo.com>
2025-07-11 12:08:33 -0500

    contact us page updated partially

---
+++ src/app/contactus/page.tsx
@@ -0,0 +1,56 @@
+'use client';
+import { useState } from "react";
+
+export default function Contactus() {
+
+    const [activeLocation, setActiveLocation] = useState<string | null>('Meadowbrook%20Manor-Naperville%2C%20Raymond%20Drive%2C%20Naperville%2C%20IL%2C%20USA');
+
+    var locations = [
+        {
+            name: "MEADOWBROOK NAPERVILLE",
+            location: '720 Raymond Drive Naperville, Illinois 60563',
+            phone: '(630) 355-0220',
+            fax: "(630) 305-6496",
+            email: 'info@meadowbrookmanor.com',
+            api: 'Meadowbrook%20Manor-Naperville%2C%20Raymond%20Drive%2C%20Naperville%2C%20IL%2C%20USA',
+            background: 'Naperville_loc'
+        }, 
+        {
+            name: "MEADOWBROOK LAGRANGE",
+            location: '339 S. 9th Avenue LaGrange, Illinois 60525',
+            phone: '(708) 354-4660',
+            fax: "(708) 354-0590",
+            email: 'info@meadowbrookmanor.com',
+            api: 'Meadowbrook%20LaGrange%2C%209th%20Avenue%2C%20La%20Grange%2C%20IL%2C%20USA',
+            background: 'Lagrange_loc'
+        },
+        {
+            name: "MEADOWBROOK BOLINGBROOK",
+            location: '431 W. Remington Blvd. Bolingbrook, Illinois 60440',
+            phone: '(630) 759-1112',
+            fax: "(630) 759-6925",
+            email: 'info@meadowbrookmanor.com',
+            api: 'Meadowbrook%20Bolingbrook%2C%20Remington%20Boulevard%2C%20Bolingbrook%2C%20IL%2C%20USA',
+            background: 'Bolingbrook_loc'
+        }
+    ]
+
+    return (
+        <div className="header_area bg-[#364635] ">
+            <h2 className="p-3 text-2xl text-center justify-center font-bold" >CONTACT US</h2>
+            <iframe className="w-full h-[50vh]" loading="lazy" allowFullScreen src={`https://www.google.com/maps/embed/v1/search?q=${activeLocation}&key=AIzaSyDJoUF3HbV8hQaZYV1qo2K3MspM5UhUJv8`}></iframe>
+            <div className="locations flex justify-center items-center h-[70vh]">
+                {locations.map((card, index) => (
+                    <div className={`card flex w-full h-full flex-col justify-center items-start ${card.background}`} onClick={() => setActiveLocation(card.api)} >
+                        <h3>{card.name}</h3>
+                        <p>{card.location}</p>
+                        <p>{card.phone}</p>
+                        <p>{card.fax}</p>
+                        <p>{card.email}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+        
+    )
+}
--- src/components/LocationOptions.tsx
+++ src/components/LocationOptions.tsx
@@ -9,7 +9,7 @@ const LocationOptions = () => {
                 <h2 className="text-3xl font-bold w-[50%] text-center self-center"  >MEADOWBROOK NAPERVILLE</h2>
                 <a className="border-3 p-2 rounded-md w-[30%]" href="">Learn more</a>
             </div>
-            <div className="Lagrance_loc w-full flex flex-col text-center align-middle justify-center items-center gap-[2vh]">
+            <div className="Lagrange_loc w-full flex flex-col text-center align-middle justify-center items-center gap-[2vh]">
                 <h2 className="text-3xl font-bold w-[50%] text-center self-center" >MEADOWBROOK LAGRANGE</h2>
                 <a className="border-3 p-2 rounded-md w-[30%]" href="">Learn more</a>
             </div>
