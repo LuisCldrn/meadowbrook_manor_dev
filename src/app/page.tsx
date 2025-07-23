459c7ee02ecbf24381cbd1da82f36bf22d7e7da5
LuisCldrn <Luis.cldrn@yahoo.com>
2025-07-22 20:59:52 -0500

    upd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <><div className="Landing flex h-[70vh] flex-col justify-center gap-4 p-10">
+    <>
+    
+    <div className="Landing flex h-[70vh] flex-col justify-center gap-4 p-10">
       <h1 className="Welcome text-5xl font-bold w-[70vw]">
         Welcome to Meadowbok Rehabilitation
       </h1>
       <p className="text-lg font-extralight w-[75vw]">At Meadowbrook, we combine expert clinical care with a warm, home-like atmosphere. Whether you're here for short-term recovery or long-term residency, you’ll be treated with dignity, compassion, and respect. Our goal is to help every individual feel comfortable, supported, and empowered to live life to the fullest.</p>
       <a className="flex w-[10vw] border-4 border-[#48a546] p-2 justify-center font-bold rounded-lg" href="">Learn More</a>
-    </div><LocationOptions />
+    </div>
+    
+    <LocationOptions />
+    
     <div className="reviews bg-[#f0f0f0] flex flex-col justify-center items-center">
       <h3 className="testimonials pt-10 text-lg text-black font-semibold">TESTIMONIALS</h3>
       <h2 className="text-black text-2xl font-bold" >WHAT OUR PATIENTS ARE SAYING</h2>
--- src/components/LocationOptions.tsx
+++ src/components/LocationOptions.tsx
@@ -1,19 +1,20 @@
 'use client';
+import styles from './LocationOptions.module.css'
 
 const LocationOptions = () => {
 
     return (
 
-        <div className="Locations flex h-[70vh]">
-            <div className="Naperville_loc flex flex-col w-full text-center align-middle content-center justify-center items-center gap-[2vh]">
+        <div className={`Locations flex h-[70vh] ${styles.location_main}`}>
+            <div className={`Naperville_loc location flex flex-col w-full text-center align-middle content-center justify-center items-center gap-[2vh] ${styles.location}`}>
                 <h2 className="text-3xl font-bold w-[50%] text-center self-center"  >MEADOWBROOK NAPERVILLE</h2>
                 <a className="border-3 p-2 rounded-md w-[30%]" href="">Learn more</a>
             </div>
-            <div className="Lagrange_loc w-full flex flex-col text-center align-middle justify-center items-center gap-[2vh]">
+            <div className={`Lagrange_loc location w-full flex flex-col text-center align-middle justify-center items-center gap-[2vh] ${styles.location}`}>
                 <h2 className="text-3xl font-bold w-[50%] text-center self-center" >MEADOWBROOK LAGRANGE</h2>
                 <a className="border-3 p-2 rounded-md w-[30%]" href="">Learn more</a>
             </div>
-            <div className="Bolingbrook_loc w-full flex flex-col text-center align-middle justify-center items-center gap-[2vh]">
+            <div className={`Bolingbrook_loc location w-full flex flex-col text-center align-middle justify-center items-center gap-[2vh] ${styles.location}`}>
                 <h2 className="text-3xl font-bold w-[50%] text-center self-center" >MEADOWBROOK BOLINGBROOK</h2>
                 <a className="border-3 p-2 rounded-md w-[30%]" href="">Learn more</a>
             </div>
